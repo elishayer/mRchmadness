c3587555cc28ce83f3779a58116b5da1660b97ae
Scott Powers <saberpowers@gmail.com>
2017-03-13 23:44:08 -0700

    added data/games2017.rds

--- R/BradleyTerry.R
+++ R/BradleyTerry.R
@@ -1,3 +1,16 @@
-BradleyTerry <- function(gameData) {
-  return(NULl)
+#' Fit a Bradley-Terry model on game score data
+#'
+#' @param game.data data.frame w/ home, home.score, away, away.score, location
+#' @returns
+BradleyTerry <- function(game.data) {
+  x = Matrix::sparseMatrix(1:nrow(games), as.numeric(as.factor(games$home))) -
+  Matrix::sparseMatrix(1:nrow(games), as.numeric(as.factor(games$away)))
+
+  y = as.numeric(games$home.score) - as.numeric(games$away.score)
+  
+  fit = glmnet::cv.glmnet(x, y, alpha = 0, standardize = FALSE,
+    lambda = exp(seq(5, -10, length = 100)))
+  beta = coef(fit, s = 'lambda.min')[, 1]
+  names(beta) = c('intercept', sort(unique(games$home)))
+  
 }
