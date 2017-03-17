c8617e210519057e36107814c7895ca9123d2902
Scott Powers <saberpowers@gmail.com>
2017-03-16 22:42:03 -0700

    update bracket scoring to work on multiple brackets and multiple results

--- R/ScoreBracket.R
+++ R/ScoreBracket.R
@@ -4,5 +4,5 @@
 #' @param result an outcome bracket of class "bracket.filled"
 #' @returns score for prediction bracket if result is what actually happens
 ScoreBracket = function(prediction, result) {
-  sum(prediction == result)
+  apply(result$winners, 2, function (x) colSums(x == prediction$winners))
 }
