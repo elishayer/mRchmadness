df4a9562ff4185b10a8a46f62ab1097cb637007d
Scott Powers <saberpowers@gmail.com>
2017-03-18 23:23:42 -0700

    changed order of arguments in scoring function

--- R/score.bracket.R
+++ R/score.bracket.R
@@ -3,6 +3,6 @@
 #' @param brackets a predicted bracket of class "bracket.filled"
 #' @param results an outcome bracket of class "bracket.filled"
 #' @returns score for prediction bracket if result is what actually happens
-score.bracket = function(brackets, results) {
-  colSums(brackets == results)
+score.bracket = function(results, brackets) {
+  colSums(results == brackets)
 }
