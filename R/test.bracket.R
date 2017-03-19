355eb8fac80ac101cd119e05d9bd6579b158c28b
Scott Powers <saberpowers@gmail.com>
2017-03-19 00:30:42 -0700

    test a bracket

---
+++ R/test.bracket.R
@@ -0,0 +1,28 @@
+#' Test a bracket
+#'
+test.bracket = function(bracket, probability.matrix, my.bracket,
+  pool.size = 30, num.sims = 1000) {
+
+  criterion = match.arg(criterion)
+  
+  num.sims = 1000
+  pool.size = 30
+  
+  pool = simulate.bracket(bracket, probability.matrix,
+    num.reps = num.sims * pool.size)
+  results = simulate.bracket(bracket, probability.matrix, num.reps = num.sims)
+  
+  score = matrix(NA, pool.size + 1, num.sims)
+  
+  for (i in 1:num.sims) {
+    brackets = cbind(my.bracket, pool[, (i - 1) * pool.size + 1:pool.size])
+    score[, i] = score.bracket(results[, i], brackets)
+  }
+  
+  rank = apply(score, 2, rank, ties.method = 'max')
+  percentile = rank[1, ] / nrow(score)
+
+  win = score[1, ] == apply(score, 2, max)
+
+  list(score = score[1, ], percentile = percentile, win = win)
+}
