31eb3f5815a1882c9d5fe33a8a590e46188d3c70
Scott Powers <saberpowers@gmail.com>
2017-03-19 00:37:05 -0700

    clean-up

--- R/sandbox-scott.R
+++ R/sandbox-scott.R
@@ -1,23 +1,29 @@
-source('R/plot.bracket.R')
+library(dplyr)
+
+source('R/fold.R')
+source('R/unfold.R')
 
 bracket = readRDS('data/bracket2017.rds')
-games = readRDS('data/games2017.rds')
+games = readRDS('data/games2017-old.rds')
 
 source('R/bradley.terry.R')
 probability.matrix = bradley.terry(games)
 
 source('R/simulate.bracket.R')
 
-time = Sys.time()
-candidates = simulate.bracket(bracket, probability.matrix, num.reps = 100000)
-Sys.time() - time
-
-time = Sys.time()
-results = simulate.bracket(bracket, probability.matrix, num.reps = 10000)
-Sys.time() - time
-
 source('R/score.bracket.R')
 ## Takes 15 minutes
 #time = Sys.time()
 #scores = score.bracket(candidates, results)
 #Sys.time() - time
+
+source('R/find.bracket.R')
+my.bracket = find.bracket(bracket, probability.matrix)
+
+source('R/plot.bracket.R')
+plot.bracket(bracket, my.bracket)
+
+source('R/test.bracket.R')
+test = test.bracket(bracket, probability.matrix, my.bracket)
+
+
