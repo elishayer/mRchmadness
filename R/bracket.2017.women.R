f1d50efbe336c49fdaaf3a6212b019a8fae1ef2f
elishayer <eshayer@stanford.edu>
2017-04-03 13:46:12 -0700

    women 2017 bracket

--- R/bracket.2017.women.R
+++ R/bracket.2017.women.R
@@ -1,7 +1,7 @@
-#' @name bracket.2017
-#' @title 2017 Men's March Madness bracket
+#' @name bracket.2017.women
+#' @title 2017 Women's March Madness bracket
 #' @description This dataset contains the ESPN team ids of the 64 teams in the
-#'   2017 March Madness men's bracket. The teams are ordered by overall seed,
+#'   2017 March Madness women's bracket. The teams are ordered by overall seed,
 #'   such that the first four team ids correspond to the four #1 seeds.
 #' @format character vector of length 64
 NULL
