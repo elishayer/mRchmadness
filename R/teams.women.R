953fa0e66dfef8ee63ffe7847769c5966c5ff380
elishayer <eshayer@stanford.edu>
2017-04-03 12:56:03 -0700

    womens teams

---
+++ R/teams.women.R
@@ -0,0 +1,7 @@
+#' @name teams
+#' @title NCAA Women's Basketball Teams
+#' @description This dataset the names of each team in NCAA Women's
+#'   Basketball from ESPN and their ESPN team id
+#' @format data frame with 413 rows and 4 variables
+#' @source \url{http://www.espn.com/womens-college-basketball/teams}
+NULL
