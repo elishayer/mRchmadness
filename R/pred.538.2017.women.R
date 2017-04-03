9faaed9ea462d3ba740724d77b2de4349c4982d5
elishayer <eshayer@stanford.edu>
2017-04-03 14:11:39 -0700

    womens 538 2017 predictions

---
+++ R/pred.538.2017.women.R
@@ -0,0 +1,9 @@
+#' @name pred.538.2017.women
+#' @title 2017 Women's March Madness 538 Win Probability Predictions
+#' @description This dataset contains the win probabilities forecasted by the
+#'   website FiveThirtyEight for the Women's March Madness tournament of the
+#'   2016-2017 season. The columns are the names of the teams, and
+#'   FiveThirtyEight's projected probability of reaching each round.
+#' @format data frame with 64 rows and 7 variables
+#' @source \url{https://projects.fivethirtyeight.com/2017-march-madness-predictions/}
+NULL
