1fdb03d67d267e868b9f5f8731758f8c98a325fc
elishayer <eshayer@stanford.edu>
2017-04-08 19:03:03 -0700

    women's population pick distribution data and doc, re-run documentation

--- R/pred.pop.men.2016.R
+++ R/pred.pop.men.2016.R
@@ -1,7 +1,7 @@
 #' @name pred.pop.men.2016
-#' @title 2016 March Madness Population Pick Distribution
+#' @title 2016 March Madness Men's Population Pick Distribution
 #' @description This dataset contains the percent of brackets submitted
-#'   to ESPN that include each of the 64 teams in March Madness 2016
+#'   to ESPN that include each of the 64 teams in Men's March Madness 2016
 #'   reaching and winning in each successive round of the tournament.
 #' @format data frame with 64 rows and 7 variables
 #' @source \url{http://games.espn.com/tournament-challenge-bracket/2016/en/whopickedwhom}
--- R/scrape.population.distribution.R
+++ R/scrape.population.distribution.R
@@ -1,21 +1,23 @@
 #' Scrape the average rate of teams being picked to win across all ESPN brackets
 #'
 #' @param year the numeric year to scrape, either 2016 or 2017
+#' @param sex either 'mens' or 'womens'
 #' @return data.frame giving percentage of population picking each team in each round
 #' @examples
 #' populationDistribution = scrape.population.distribution(2017)
 #' @export
 #' @author eshayer
-scrape.population.distribution = function(year) {
-
+scrape.population.distribution = function(year, sex = c('mens', 'womens')) {
+  sex = match.arg(sex)
   `%>%` = dplyr::`%>%`
 
   if (!(year %in% c(2016, 2017))) {
     stop(paste0('The year ', year, ' is not available'))
   }
 
-  url = paste0('http://games.espn.com/tournament-challenge-bracket/', year,
-    '/en/whopickedwhom')
+  url = paste0('http://games.espn.com/tournament-challenge-bracket',
+               ifelse(sex == 'mens', '', '-women'), '/', year,
+               '/en/whopickedwhom')
   
   cells = xml2::read_html(url) %>%
     rvest::html_nodes('table.wpw-table td')
