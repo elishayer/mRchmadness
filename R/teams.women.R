39f4c12565eab1a8494c2c9e18abc880d7d8a491
elishayer <eshayer@stanford.edu>
2017-04-08 19:51:18 -0700

    womens team names for pred.pop and pred.538

--- R/teams.women.R
+++ R/teams.women.R
@@ -1,7 +1,8 @@
 #' @name teams.women
 #' @title NCAA Women's Basketball Teams
 #' @description This dataset the names of each team in NCAA Women's
-#'   Basketball from ESPN and their ESPN team id
-#' @format data frame with 413 rows and 4 variables
+#'   Basketball from ESPN, their ESPN team id, their name as it
+#'   appears on the Who Picked Whom, and their FiveThirtyEight name.
+#' @format data frame with 349 rows and 4 variables
 #' @source \url{http://www.espn.com/womens-college-basketball/teams}
 NULL
