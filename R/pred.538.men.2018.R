7cf17522df93b36131f70d4e6acb0b2589b3a981
elishayer <eshayer@stanford.edu>
2018-03-11 18:17:43 -0700

    #9 men's 538 predictions, update name.538 in teams.men

---
+++ R/pred.538.men.2018.R
@@ -0,0 +1,10 @@
+#' @name pred.538.men.2018
+#' @title 2018 March Madness 538 Win Probability Predictions
+#' @description This dataset contains the win probabilities forecasted
+#'   by the website FiveThirtyEight before any games have been played
+#'   in the March Madness tournament of the 2017-2018 season. The columns
+#'   are the names of the teams, and FiveThirtyEight's projected
+#'   probability of reaching each round of the tournament.
+#' @format data frame with 68 rows and 7 variables
+#' @source \url{https://projects.fivethirtyeight.com/2018-march-madness-predictions/}
+NULL
--- R/teams.men.R
+++ R/teams.men.R
@@ -4,7 +4,7 @@
 #'   Basketball from ESPN, their ESPN team id, their name as it
 #'   appears on the Who Picked Whom, and the FiveThirtyEight name.
 #'   The last two name fields are only available for those teams
-#'   who played in March Madness in 2016 or 2017, and are otherwise
+#'   who played in March Madness in 2016 to 2018, and are otherwise
 #'   NA.
 #' @format data frame with 351 rows and 4 variables
 #' @source \url{http://www.espn.com/mens-college-basketball/teams}
