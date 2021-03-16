b5b0b1dbf8cc029d0d5739d1b7e81da5057d6696
elishayer <eshayer@stanford.edu>
2021-03-15 19:44:21 -0500

    clean up documentation with roxygen

--- R/pred.pop.men.2018.R
+++ R/pred.pop.men.2018.R
@@ -1,7 +1,7 @@
 #' @name pred.pop.men.2018
 #' @title 2018 March Madness Population Pick Distribution
 #' @description This dataset contains the percent of brackets submitted
-#'   to ESPN that include each of the 64 teams in March Madness 2017
+#'   to ESPN that include each of the 64 teams in March Madness 2018
 #'   reaching and winning in each successive round of the tournament.
 #'   Accessed the evening of March 11th, 2018 (Selection Sunday).
 #' @format data frame with 64 rows and 7 variables
