0e38c24cb7dbc1dc48dccde9e9a1e9214693c749
elishayer <eshayer@stanford.edu>
2021-03-18 23:45:01 -0500

    fix #29 add pred.538.men.2021 (from @gheemony), update teams.men to match

---
+++ R/pred.538.men.2021.R
@@ -0,0 +1,11 @@
+#' @name pred.538.men.2021
+#' @title 2018 March Madness 538 Win Probability Predictions
+#' @description This dataset contains the win probabilities forecasted
+#'   by the website FiveThirtyEight before any games have been played
+#'   in the March Madness tournament of the 2020-2021 season. The columns
+#'   are the names of the teams, and FiveThirtyEight's projected
+#'   probability of reaching each round of the tournament. The data was pulled
+#'   and formatted by @gheemony.
+#' @format data frame with 64 rows and 7 variables
+#' @source \url{https://projects.fivethirtyeight.com/2021-march-madness-predictions/}
+NULL
