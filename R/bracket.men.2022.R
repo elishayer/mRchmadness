b39921d647f7e343fe40e52d9755ddf5264c1e8b
elishayer <eshayer@stanford.edu>
2022-03-15 09:52:57 -0700

    #30 2022 men's bracket and games

---
+++ R/bracket.men.2022.R
@@ -0,0 +1,7 @@
+#' @name bracket.men.2022
+#' @title 2022 Men's March Madness bracket
+#' @description This dataset contains the ESPN team ids of the 64 teams in the
+#'   2022 March Madness men's bracket. The teams are ordered by overall seed,
+#'   such that the first four team ids correspond to the four #1 seeds.
+#' @format character vector of length 64
+NULL
