9490ecc49c7429f558f32340b4e9ebe5f0497e74
cbranch117 <cbranch2@wisc.edu>
2022-03-16 21:33:03 -0500

    add stuff

---
+++ R/pred.538.men.2022.R
@@ -0,0 +1,11 @@
+#' @name pred.538.men.2022
+#' @title 2018 March Madness 538 Win Probability Predictions
+#' @description This dataset contains the win probabilities forecasted
+#'   by the website FiveThirtyEight before any games have been played
+#'   in the March Madness tournament of the 2021-2022 season. The columns
+#'   are the names of the teams, and FiveThirtyEight's projected
+#'   probability of reaching each round of the tournament. The data was pulled
+#'   and formatted by @gheemony.
+#' @format data frame with 64 rows and 7 variables
+#' @source \url{https://projects.fivethirtyeight.com/2022-march-madness-predictions/}
+NULL
