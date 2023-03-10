61624ba858ade35d690bf48c3632d801658de3fe
Scott Powers <saberpowers@gmail.com>
2023-03-10 17:49:35 -0600

    add bracket.women.2022

--- R/bracket.women.R
+++ R/bracket.women.R
@@ -21,3 +21,11 @@ NULL
 #'   such that the first four team ids correspond to the four #1 seeds.
 #' @format character vector of length 64
 NULL
+
+#' @name bracket.women.2022
+#' @title 2022 Women's March Madness bracket
+#' @description This dataset contains the ESPN team ids of the 64 teams in the
+#'   2022 March Madness women's bracket. The teams are ordered by overall seed,
+#'   such that the first four team ids correspond to the four #1 seeds.
+#' @format character vector of length 64
+NULL
