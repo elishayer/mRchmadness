b9db0046f888d3be8ff8b08f8dbb1ca9068ce4c0
Scott Powers <saberpowers@gmail.com>
2023-03-11 15:27:43 -0600

    add man file for bracket.women.2022

--- R/pred.kenpom.men.R
+++ R/pred.kenpom.men.R
@@ -8,3 +8,14 @@
 #' @format data frame with 64 rows and 7 variables
 #' @source \url{https://kenpom.com/blog/2018-ncaa-tournament-probabilities/}
 NULL
+
+#' @name pred.kenpom.men.2019
+#' @title 2019 March Madness KenPom Win Probability Predictions
+#' @description This dataset contains the win probabilities forecasted
+#'   by the website KenPom before any games have been played
+#'   in the March Madness tournament of the 2018-2019 season. The columns
+#'   are the names of the teams, and KenPom's projected
+#'   probability of reaching each round of the tournament.
+#' @format data frame with 64 rows and 7 variables
+#' @source \url{https://kenpom.com/blog/2019-ncaa-tournament-probabilities/}
+NULL
