3f193805fe4c198efc490ab480d907d46edbb72b
Scott Powers <saberpowers@gmail.com>
2023-03-10 18:11:04 -0600

    Update how we scrape game.id

--- R/scrape.game.results.R
+++ R/scrape.game.results.R
@@ -196,8 +196,8 @@ scrape.team.game.results = function(year, team.id, league) {
   game.id = result.cells %>%
     rvest::html_node('.ml4 a') %>%
     rvest::html_attr('href') %>%
-    strsplit('=') %>%
-    sapply(function(row) row[2])
+    # Extract all numbers from the URL game link (assume this is the game ID)
+    gsub(".*?([0-9]+).*", "\\1", .)
   
   data.frame(game.id = game.id,
              primary.id = team.id,
