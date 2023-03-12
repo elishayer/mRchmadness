b7c0a09c9aeabdf9005f99379a876c88e33d8588
Scott Powers <saberpowers@gmail.com>
2023-03-11 19:34:04 -0600

    add 2022 kenpom predictions

--- R/pred.kenpom.men.R
+++ R/pred.kenpom.men.R
@@ -30,3 +30,14 @@ NULL
 #' @format data frame with 64 rows and 7 variables
 #' @source \url{https://kenpom.com/blog/ncaa-tourney-forecast/}
 NULL
+
+#' @name pred.kenpom.men.2022
+#' @title 2022 March Madness KenPom Win Probability Predictions
+#' @description This dataset contains the win probabilities forecasted
+#'   by the website KenPom before any games have been played
+#'   in the March Madness tournament of the 2021-2022 season. The columns
+#'   are the names of the teams, and KenPom's projected
+#'   probability of reaching each round of the tournament.
+#' @format data frame with 64 rows and 7 variables
+#' @source \url{https://kenpom.com/blog/2022-ncaa-tournament-probabilities/}
+NULL
