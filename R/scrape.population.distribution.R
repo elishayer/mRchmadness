efbc9c55f4043a97b2bcb056b190fd442cfd2adf
Scott Powers <saberpowers@gmail.com>
2023-03-12 15:56:41 -0500

    add 2022 womens population picks

--- R/scrape.population.distribution.R
+++ R/scrape.population.distribution.R
@@ -1,6 +1,6 @@
 #' Scrape the average rate of teams being picked to win across all ESPN brackets
 #'
-#' @param year the numeric year to scrape, in the range 2016 to 2018
+#' @param year the numeric year to scrape
 #' @param league either 'mens' or 'womens'
 #' @return data.frame giving percentage of population picking each team in each round
 #' @examples
@@ -11,7 +11,7 @@ scrape.population.distribution = function(year, league = c('mens', 'womens')) {
   league = match.arg(league)
   `%>%` = dplyr::`%>%`
 
-  if (!(year %in% c(2016, 2017, 2018, 2019, 2021))) {
+  if (!(year %in% c(2016, 2017, 2018, 2019, 2021, 2022))) {
     stop(paste0('The year ', year, ' is not available'))
   }
 
