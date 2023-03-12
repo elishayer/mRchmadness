540452e698799074e06465955ba5f70c5ef8abbb
Scott Powers <saberpowers@gmail.com>
2023-03-11 20:33:26 -0600

    add 2022 mens population picks

--- R/pred.pop.men.R
+++ R/pred.pop.men.R
@@ -35,3 +35,13 @@ NULL
 #' @format data frame with 64 rows and 7 variables
 #' @source \url{http://games.espn.com/tournament-challenge-bracket/2021/en/whopickedwhom}
 NULL
+
+#' @name pred.pop.men.2022
+#' @title 2022 March Madness Population Pick Distribution
+#' @description This dataset contains the percent of brackets submitted
+#'   to ESPN that include each of the 64 teams in March Madness 2022
+#'   reaching and winning in each successive round of the tournament.
+#'   Accessed the evening of March 11th, 2023.
+#' @format data frame with 64 rows and 7 variables
+#' @source \url{http://games.espn.com/tournament-challenge-bracket/2022/en/whopickedwhom}
+NULL
