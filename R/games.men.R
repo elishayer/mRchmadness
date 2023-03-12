99f9e7aa14f20a8d62125ec0f3492d95d7e9f3c3
Scott Powers <saberpowers@gmail.com>
2023-03-12 17:00:29 -0500

    add 2022-23 mens games data

--- R/games.men.R
+++ R/games.men.R
@@ -62,3 +62,16 @@ NULL
 #' @format data frame with 5835 rows and 7 variables
 #' @source ESPN
 NULL
+
+#' @name games.men.2023
+#' @title 2023 NCAA Men's Basketball Game-by-Game Results
+#' @description This dataset contains the game results of the
+#'   college men's basketball 2022-2023 season, identifying
+#'   the game, the home and away teams, (using the ESPN ID system),
+#'   the home and away scores, whether the game was played at a
+#'   neutral arena, and whether the game went into overtime. Teams
+#'   that are not in Division I are uniformly identified by the
+#'   string 'NA' as their id.
+#' @format data frame with 6108 rows and 7 variables
+#' @source ESPN
+NULL
