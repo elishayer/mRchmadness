29c346a9386bc803192d4385ad89ba9ff6c534d5
Scott Powers <saberpowers@gmail.com>
2025-03-17 00:34:55 -0500

    update roxygen for data documentation

--- R/games.men.R
+++ R/games.men.R
@@ -88,3 +88,16 @@ NULL
 #' @format data frame with 5619 rows and 7 variables
 #' @source ESPN
 NULL
+
+#' @name games.men.2025
+#' @title 2025 NCAA Men's Basketball Game-by-Game Results
+#' @description This dataset contains the game results of the
+#'   college men's basketball 2024-2025 season, identifying
+#'   the game, the home and away teams, (using the ESPN ID system),
+#'   the home and away scores, whether the game was played at a
+#'   neutral arena, and whether the game went into overtime. Teams
+#'   that are not in Division I are uniformly identified by the
+#'   string 'NA' as their id.
+#' @format data frame with 6168 rows and 7 variables
+#' @source ESPN
+NULL
--- R/pred.pop.women.R
+++ R/pred.pop.women.R
@@ -35,3 +35,13 @@ NULL
 #' @format data frame with 64 rows and 9 variables
 #' @source \url{https://gambit-api.fantasy.espn.com/apis/v1/propositions?challengeId=241}
 NULL
+
+#' @name pred.pop.women.2025
+#' @title 2025 March Madness Population Pick Distribution
+#' @description This dataset contains the percent of brackets submitted
+#'   to ESPN that include each of the 64 teams in March Madness 2025
+#'   reaching and winning in each successive round of the tournament.
+#'   Accessed the evening of March 17, 2025.
+#' @format data frame with 64 rows and 9 variables
+#' @source \url{https://gambit-api.fantasy.espn.com/apis/v1/propositions?challengeId=241}
+NULL
